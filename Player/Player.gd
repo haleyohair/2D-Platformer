c0757f3b2f358477a2ceebf4e78f20aa8bf19860
haleyohair <halohair@iu.edu>
2023-10-30 19:54:32 -0400

    tries to add enemies and player functions

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,11 +2,20 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -800.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func die():
+	queue_free()
+	
+func attack():
+	if $Attack.is_colliding():
+		var target = $Attack.get_collider()
+		print(target)
+		if target.has_method("damage"):
+			target.damage()
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -22,6 +31,9 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$Sprite.play("Jump")
+		
+	if Input.is_action_just_pressed("Attack") and is_on_floor():
+		$Sprite.play("Attack")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
