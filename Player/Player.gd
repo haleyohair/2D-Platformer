4c6b0c7f71da0b9f03e2b6e2e2373d0a2ecb4076
haleyohair <halohair@iu.edu>
2023-10-31 17:27:24 -0400

    music and sound

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,7 +8,9 @@ const JUMP_VELOCITY = -800.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func die():
+	
 	queue_free()
+
 	
 func attack():
 	if $Attack.is_colliding():
@@ -24,6 +26,8 @@ func _physics_process(delta):
 		$Sprite.animation="Fall"
 	elif abs(velocity.x)>0 and $Sprite.animation !="Walk":
 		$Sprite.play("Walk")
+		var player_walk= get_node("/root/Game/Player_Walk")
+		player_walk.play()
 	elif velocity.x==0:
 		$Sprite.animation="Idle"
 		
@@ -31,6 +35,8 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$Sprite.play("Jump")
+		var player_jump= get_node("/root/Game/Player_Jump")
+		player_jump.play()
 		
 	if Input.is_action_just_pressed("Attack") and is_on_floor():
 		$Sprite.play("Attack")
@@ -45,5 +51,7 @@ func _physics_process(delta):
 	else:
 		$Sprite.flip_h=false;
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if Input.is_action_just_pressed("Left") or ("Right") and is_on_floor():
+		$Grass.emitting=true
 	
 	move_and_slide()
