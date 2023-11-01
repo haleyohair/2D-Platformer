0057c36766d5e7661d8adc08b2bad28599bc2963
Jason Francis <jarfranc@iu.edu>
2023-10-31 20:44:34 -0400

    Just a little clean up
    This wasn't too bad. Hopefully my changes are helpful. Let me know if I can do anything else. Looks great so far!

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = -800.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var attacking = false
 
 func die():
 	queue_free()
@@ -13,27 +14,36 @@ func die():
 func attack():
 	if $Attack.is_colliding():
 		var target = $Attack.get_collider()
-		print(target)
 		if target.has_method("damage"):
 			target.damage()
+
+func set_animation(a):
+	if $Sprite.animation and not attacking:
+		if a == "Attack": 
+			$Sprite.offset = Vector2(2,8)
+			attacking = true
+			$Attacking.start()
+		else: $Sprite.offset = Vector2.ZERO
+		$Sprite.play(a)
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		$Sprite.animation="Fall"
+		set_animation("Fall")
 	elif abs(velocity.x)>0 and $Sprite.animation !="Walk":
-		$Sprite.play("Walk")
+		set_animation("Walk")
 	elif velocity.x==0:
-		$Sprite.animation="Idle"
+		set_animation("Idle")
 		
 	# Handle Jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		$Sprite.play("Jump")
+		set_animation("Jump")
 		
 	if Input.is_action_just_pressed("Attack") and is_on_floor():
-		$Sprite.play("Attack")
+		set_animation("Attack")
+		attack()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -47,3 +57,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	move_and_slide()
+
+
+func _on_attacking_timeout():
+	attacking = false
